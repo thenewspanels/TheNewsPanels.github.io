55101b4d39d4bc19ab7c24b4e9be611ac5d7200f
Travis CI User <travis@example.org>
2019-07-25 14:47:15 +0000

    Auto-commit: makefile

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('//service-worker.js?v=1563979632').then(function(reg) {
+            navigator.serviceWorker.register('//service-worker.js?v=1564066034').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
